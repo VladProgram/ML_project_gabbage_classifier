e67e54dfcdd2f5ac48e1f554a23c06ea2ceb33e8
Iva <iva@popova.com>
2023-03-14 20:51:10 +0200

    change script to convert video to frames

--- src/rename.py
+++ src/rename.py
@@ -1,20 +1,24 @@
 import os
 counter = 0
-files = []
-file_names = os.listdir(r'data\frames')
+folders = []
+file_names = os.listdir('data/frames')
 for i in file_names:
-    path = r'data\frames\{}'.format(i)
-    files.append(path)
+    path = f'data/frames/{i}'
+    folders.append(path)
     # print(path)
 
-for ix, file in enumerate(files):
-    images = os.listdir(file)
+
+
+for ix, folder_name in enumerate(folders):
+    images = os.listdir(folder_name)
     for img_n, img in enumerate(images):
-        img_path = file + '\\' + img
-        new_path = file + '\\' + r'{}_{}.jpg'.format(ix, counter)
-        os.rename(img_path, new_path)
+        # img_path = folder_name + '\\' + img
+        img_path = os.path.join(folder_name, img)
+        # new_path = folder_name + '\\' + f'{ix}_{counter}.jpg'
+        new_path = os.path.join(folder_name, f'{ix}_{counter}.jpg')
+        # os.rename(img_path, new_path)
+        print(img_path, new_path)
         counter += 1
-        print(img_path, counter)
-        
-        
-        
+
+
+
